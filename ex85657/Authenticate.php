29af2c4f63f80d3f372d0c5f11c5565cf5814aab
Tyiah <105523967+Tyiah@users.noreply.github.com>
2022-05-16 17:00:36 +0200

    Ex

---
+++ ex85657/Authenticate.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+require 'config.php';
+
+// haal gegevens van AJAX op
+if($_POST['type']==1){
+    $username= $_POST['studentnumber'];
+    $password= SHA1($_POST['password']);
+
+    // check of de form velden zijn ingevuld
+    if ( !isset($username, $password) ) {
+        exit('Vul beide velden in!');
+    }
+
+    // Bereid de SQL voor
+    if ($stmt = $con->prepare('SELECT Naam, Wachtwoord FROM Studenten WHERE StudentNummer = ?')) {
+        // Bindparameters (s = string, i = int, b = blob, etc), in dit geval is de gebruikersnaam een string, dus "s"
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        // Sla het resultaat op zodat we kunnen controleren of het account in de database bestaat.
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($name, $passwordResult);
+            $stmt->fetch();
+            // Account bestaat, test of het wachtwoord klopt.
+            if ($password == $passwordResult) {
+                // Maak sessions aan en sla dit op in de cookies om het terug te krijgen
+                session_regenerate_id();
+                $_SESSION['loggedin'] = TRUE;
+                $_SESSION['studentnumber'] = $username;
+                $_SESSION['name'] = $name;
+
+                echo json_encode(array("statusCode"=>200));
+
+
+            } else {
+                // Verkeerd wachtwoord
+                echo json_encode(array("statusCode"=>201));
+            }
+        } else {
+            // Verkeerd gebruikersnaam
+            echo json_encode(array("statusCode"=>202));
+        }
+
+        $stmt->close();
+    }
+}
+?>
---
+++ ex85657/dashboard/home.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+
+require '../config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Home Page</title>
+    <!-- Imports -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Css -->
+    <link type="text/css" rel="stylesheet" href="../css/home-style.css">
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../js/jquery-3.6.0.min.js"></script>
+</head>
+<body class="loggedin">
+<nav class="navtop">
+    <div class="navtop-header">
+        <img src="../img/glr.png" style="height: 100%;padding: 0px 50px">
+        <div class="navbar-right">
+            <a href="home.php"><i class="fas fa-user-circle"></i>Startpagina</a>
+            <?php
+            if (isset($_SESSION['loggedin'])) {
+                ?>
+                <a href="../logout.php"><i class="fas fa-sign-out-alt"></i>Log uit</a>
+                <?php
+            } else {
+                ?>
+                <a href="../Index.html"><i class="fas fa-sign-out-alt"></i>Log in</a>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+</nav>
+<div class="content">
+    <h2>Ga Lekker Reizen</h2>
+    <p>Hi, <?=$_SESSION['name']?>!</p>
+    <div class="trip-container">
+    <?php
+    if (isset($_SESSION['loggedin'])) {
+        $result = $con->query('SELECT * FROM Reizen');
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {?>
+                <div class="trip-box">
+                    <div>
+                        <img src="../img/texel-eiland-vuurtoren-809x540.jpeg" class="trip-box-img">
+                        <div class="trip-box-text">
+                            <h2><?php echo $row["Titel"];?></h2>
+                            <p><?php echo $row["Bestemming"];?></p>
+                            <p><?php echo $row["Begindatum"];?></p>
+                            <?php echo "<a href='#editEmployeeModal".$row['ID']."' data-toggle='modal' class='trip-box-button'>Meer Informatie</a>";?>
+                        </div>
+                    </div>
+                </div>
+
+        <!-- Edit Modal HTML -->
+        <?php echo "<div id='editEmployeeModal".$row['ID']."' class='modal fade'>";?>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <img src="../img/texel-eiland-vuurtoren-809x540.jpeg" class="trip-box-img">
+                    <div class="trip-box-text">
+                        <h2><?php echo $row["Titel"];?></h2>
+                        <p><?php echo $row["Bestemming"];?></p>
+                        <p><?php echo $row["Omschrijving"];?></p>
+                        <p><strong><?php echo $row["Begindatum"];?></strong> tot <strong><?php echo $row["Einddatum"];?></strong></p>
+                        <p><?php echo $row["Begindatum"];?></p>
+                    </div>
+                </div>
+            </div>
+                </div>
+        <?php
+            }
+        }
+    }
+    ?>
+    </div>
+</div>
+</body>
+</html>
